b3ad3e8341ddbd2fb38386852df7ae3bcb5d297a
fgaia <88512239+fgaia@users.noreply.github.com>
2021-08-05 18:30:54 -0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,293 @@
+﻿
+namespace FEpimpMyunit
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picBox6 = new System.Windows.Forms.PictureBox();
+            this.picBox10 = new System.Windows.Forms.PictureBox();
+            this.picBox5 = new System.Windows.Forms.PictureBox();
+            this.picBox9 = new System.Windows.Forms.PictureBox();
+            this.picBox4 = new System.Windows.Forms.PictureBox();
+            this.picBox3 = new System.Windows.Forms.PictureBox();
+            this.picBox8 = new System.Windows.Forms.PictureBox();
+            this.picBox2 = new System.Windows.Forms.PictureBox();
+            this.picBox7 = new System.Windows.Forms.PictureBox();
+            this.picBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.cboSerie = new System.Windows.Forms.ComboBox();
+            this.cboQuantidade = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox10)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picBox6
+            // 
+            this.picBox6.Location = new System.Drawing.Point(30, 385);
+            this.picBox6.Name = "picBox6";
+            this.picBox6.Size = new System.Drawing.Size(52, 51);
+            this.picBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBox6.TabIndex = 1;
+            this.picBox6.TabStop = false;
+            // 
+            // picBox10
+            // 
+            this.picBox10.Location = new System.Drawing.Point(313, 385);
+            this.picBox10.Name = "picBox10";
+            this.picBox10.Size = new System.Drawing.Size(52, 51);
+            this.picBox10.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBox10.TabIndex = 2;
+            this.picBox10.TabStop = false;
+            // 
+            // picBox5
+            // 
+            this.picBox5.Location = new System.Drawing.Point(313, 307);
+            this.picBox5.Name = "picBox5";
+            this.picBox5.Size = new System.Drawing.Size(52, 51);
+            this.picBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBox5.TabIndex = 3;
+            this.picBox5.TabStop = false;
+            // 
+            // picBox9
+            // 
+            this.picBox9.Location = new System.Drawing.Point(242, 385);
+            this.picBox9.Name = "picBox9";
+            this.picBox9.Size = new System.Drawing.Size(52, 51);
+            this.picBox9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBox9.TabIndex = 4;
+            this.picBox9.TabStop = false;
+            // 
+            // picBox4
+            // 
+            this.picBox4.Location = new System.Drawing.Point(242, 307);
+            this.picBox4.Name = "picBox4";
+            this.picBox4.Size = new System.Drawing.Size(52, 51);
+            this.picBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBox4.TabIndex = 5;
+            this.picBox4.TabStop = false;
+            // 
+            // picBox3
+            // 
+            this.picBox3.Location = new System.Drawing.Point(172, 307);
+            this.picBox3.Name = "picBox3";
+            this.picBox3.Size = new System.Drawing.Size(52, 51);
+            this.picBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBox3.TabIndex = 6;
+            this.picBox3.TabStop = false;
+            // 
+            // picBox8
+            // 
+            this.picBox8.Location = new System.Drawing.Point(172, 385);
+            this.picBox8.Name = "picBox8";
+            this.picBox8.Size = new System.Drawing.Size(52, 51);
+            this.picBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBox8.TabIndex = 7;
+            this.picBox8.TabStop = false;
+            // 
+            // picBox2
+            // 
+            this.picBox2.Location = new System.Drawing.Point(102, 307);
+            this.picBox2.Name = "picBox2";
+            this.picBox2.Size = new System.Drawing.Size(52, 51);
+            this.picBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBox2.TabIndex = 8;
+            this.picBox2.TabStop = false;
+            // 
+            // picBox7
+            // 
+            this.picBox7.Location = new System.Drawing.Point(102, 385);
+            this.picBox7.Name = "picBox7";
+            this.picBox7.Size = new System.Drawing.Size(52, 51);
+            this.picBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBox7.TabIndex = 9;
+            this.picBox7.TabStop = false;
+            // 
+            // picBox1
+            // 
+            this.picBox1.Location = new System.Drawing.Point(30, 307);
+            this.picBox1.Name = "picBox1";
+            this.picBox1.Size = new System.Drawing.Size(52, 51);
+            this.picBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBox1.TabIndex = 10;
+            this.picBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(158, 242);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(81, 33);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // cboSerie
+            // 
+            this.cboSerie.FormattingEnabled = true;
+            this.cboSerie.Items.AddRange(new object[] {
+            "Fire Emblem Echoes (Alm)"});
+            this.cboSerie.Location = new System.Drawing.Point(143, 149);
+            this.cboSerie.Name = "cboSerie";
+            this.cboSerie.Size = new System.Drawing.Size(121, 21);
+            this.cboSerie.TabIndex = 12;
+            // 
+            // cboQuantidade
+            // 
+            this.cboQuantidade.FormattingEnabled = true;
+            this.cboQuantidade.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10"});
+            this.cboQuantidade.Location = new System.Drawing.Point(143, 209);
+            this.cboQuantidade.Name = "cboQuantidade";
+            this.cboQuantidade.Size = new System.Drawing.Size(121, 21);
+            this.cboQuantidade.TabIndex = 13;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(176, 130);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 16);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Series";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(130, 187);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(147, 16);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Quantity of characters";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::FEpimpMyunit.Properties.Resources.Fire_Emblem_logo_svg;
+            this.pictureBox1.Location = new System.Drawing.Point(61, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(283, 100);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 16;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("ClassicXtraRoundW00-Heavy", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(143, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(121, 20);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Pimp My Unit";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(390, 470);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cboQuantidade);
+            this.Controls.Add(this.cboSerie);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.picBox1);
+            this.Controls.Add(this.picBox7);
+            this.Controls.Add(this.picBox2);
+            this.Controls.Add(this.picBox8);
+            this.Controls.Add(this.picBox3);
+            this.Controls.Add(this.picBox4);
+            this.Controls.Add(this.picBox9);
+            this.Controls.Add(this.picBox5);
+            this.Controls.Add(this.picBox10);
+            this.Controls.Add(this.picBox6);
+            this.Name = "Form1";
+            this.Text = "Fire Emblem PMU";
+            ((System.ComponentModel.ISupportInitialize)(this.picBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox10)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picBox6;
+        private System.Windows.Forms.PictureBox picBox10;
+        private System.Windows.Forms.PictureBox picBox5;
+        private System.Windows.Forms.PictureBox picBox9;
+        private System.Windows.Forms.PictureBox picBox4;
+        private System.Windows.Forms.PictureBox picBox3;
+        private System.Windows.Forms.PictureBox picBox8;
+        private System.Windows.Forms.PictureBox picBox2;
+        private System.Windows.Forms.PictureBox picBox7;
+        private System.Windows.Forms.PictureBox picBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox cboSerie;
+        private System.Windows.Forms.ComboBox cboQuantidade;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FEpimpMyunit
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void erasePaths()
+        {
+            picBox1.ImageLocation = "";
+            picBox2.ImageLocation = "";
+            picBox3.ImageLocation = "";
+            picBox4.ImageLocation = "";
+            picBox5.ImageLocation = "";
+            picBox6.ImageLocation = ""; 
+            picBox7.ImageLocation = "";
+            picBox8.ImageLocation = "";
+            picBox9.ImageLocation = "";
+            picBox10.ImageLocation = "";
+        }
+
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (cboQuantidade.Text != "" )
+            {
+                int caseSwitch = int.Parse(cboQuantidade.Text);
+                erasePaths();
+
+                if (cboSerie.Text == "Fire Emblem Echoes (Alm)")
+                {
+                    switch (caseSwitch)
+                    {
+                        case 1:
+                            definepic1();
+                            break;
+
+                        case 2:
+                            definepic1();
+                            definepic2();
+                            break;
+
+                        case 3:
+                            definepic1();
+                            definepic2();
+                            definepic3();
+                            break;
+
+                        case 4:
+                            definepic1();
+                            definepic2();
+                            definepic3();
+                            definepic4();
+                            break;
+
+                        case 5:
+                            definepic1();
+                            definepic2();
+                            definepic3();
+                            definepic4();
+                            definepic5();
+                            break;
+
+                        case 6:
+                            definepic1();
+                            definepic2();
+                            definepic3();
+                            definepic4();
+                            definepic5();
+                            definepic6();
+                            break;
+
+                        case 7:
+                            definepic1();
+                            definepic2();
+                            definepic3();
+                            definepic4();
+                            definepic5();
+                            definepic6();
+                            definepic7();
+                            break;
+
+                        case 8:
+                            definepic1();
+                            definepic2();
+                            definepic3();
+                            definepic4();
+                            definepic5();
+                            definepic6();
+                            definepic7();
+                            definepic8();
+                            break;
+
+                        case 9:
+                            definepic1();
+                            definepic2();
+                            definepic3();
+                            definepic4();
+                            definepic5();
+                            definepic6();
+                            definepic7();
+                            definepic8();
+                            definepic9();
+                            break;
+
+                        case 10:
+                            definepic1();
+                            definepic2();
+                            definepic3();
+                            definepic4();
+                            definepic5();
+                            definepic6();
+                            definepic7();
+                            definepic8();
+                            definepic9();
+                            definepic10();
+                            break;
+                    }
+
+                }
+                else { MessageBox.Show("You must choose a series!", "Alert"); }
+
+
+            } else { MessageBox.Show("You must select characters quantity!", "alert"); }
+
+        }
+
+        public void definepic1()
+        {
+                string[] almchar = new string[] {"Gray", "Tobin", "Faye", "Kliff", "Lukas", "Silque", "Clair", "Clive",
+                "Forsyth", "Python", "Luthier", "Mathilda", "Delthea", "Tatiana", "Zeke", "Mycen"};
+                Random rnd = new Random();
+                picBox1.ImageLocation = @"C:\Users\felip\source\repos\FEpimpMyunit\Resources\fire emblem images\" + almchar[rnd.Next(almchar.Length)] + ".png";
+         }
+
+        public void definepic2()
+        {
+            string[] almchar = new string[] {"Gray", "Tobin", "Faye", "Kliff", "Lukas", "Silque", "Clair", "Clive",
+                "Forsyth", "Python", "Luthier", "Mathilda", "Delthea", "Tatiana", "Zeke", "Mycen"};
+            Random rnd = new Random();
+
+            for (picBox2.ImageLocation = @"C:\Users\felip\source\repos\FEpimpMyunit\Resources\fire emblem images\" + almchar[rnd.Next(almchar.Length)] + ".png";
+            picBox2.ImageLocation == picBox1.ImageLocation; picBox2.ImageLocation = @"C:\Users\felip\source\repos\FEpimpMyunit\Resources\fire emblem images\" + almchar[rnd.Next(almchar.Length)] + ".png") { }
+        }
+
+        public void definepic3()
+        {
+            string[] almchar = new string[] {"Gray", "Tobin", "Faye", "Kliff", "Lukas", "Silque", "Clair", "Clive",
+                "Forsyth", "Python", "Luthier", "Mathilda", "Delthea", "Tatiana", "Zeke", "Mycen"};
+            Random rnd = new Random();
+            for (picBox3.ImageLocation = @"C:\Users\felip\source\repos\FEpimpMyunit\Resources\fire emblem images\" + almchar[rnd.Next(almchar.Length)] + ".png";
+                           picBox3.ImageLocation == picBox1.ImageLocation || picBox3.ImageLocation == picBox2.ImageLocation; 
+                           picBox3.ImageLocation = @"C:\Users\felip\source\repos\FEpimpMyunit\Resources\fire emblem images\" + almchar[rnd.Next(almchar.Length)] + ".png") { }
+        }
+
+        public void definepic4()
+        {
+            string[] almchar = new string[] {"Gray", "Tobin", "Faye", "Kliff", "Lukas", "Silque", "Clair", "Clive",
+                "Forsyth", "Python", "Luthier", "Mathilda", "Delthea", "Tatiana", "Zeke", "Mycen"};
+            Random rnd = new Random();
+            for (picBox4.ImageLocation = @"C:\Users\felip\source\repos\FEpimpMyunit\Resources\fire emblem images\" + almchar[rnd.Next(almchar.Length)] + ".png";
+                           picBox4.ImageLocation == picBox1.ImageLocation || picBox4.ImageLocation == picBox2.ImageLocation || picBox4.ImageLocation == picBox3.ImageLocation;
+                           picBox4.ImageLocation = @"C:\Users\felip\source\repos\FEpimpMyunit\Resources\fire emblem images\" + almchar[rnd.Next(almchar.Length)] + ".png") { }
+         }
+
+        public void definepic5()
+        {
+            string[] almchar = new string[] {"Gray", "Tobin", "Faye", "Kliff", "Lukas", "Silque", "Clair", "Clive",
+                "Forsyth", "Python", "Luthier", "Mathilda", "Delthea", "Tatiana", "Zeke", "Mycen"};
+            Random rnd = new Random();
+            for (picBox5.ImageLocation = @"C:\Users\felip\source\repos\FEpimpMyunit\Resources\fire emblem images\" + almchar[rnd.Next(almchar.Length)] + ".png";
+                           picBox5.ImageLocation == picBox1.ImageLocation || picBox5.ImageLocation == picBox2.ImageLocation || picBox5.ImageLocation == picBox3.ImageLocation || picBox5.ImageLocation == picBox4.ImageLocation;
+                           picBox5.ImageLocation = @"C:\Users\felip\source\repos\FEpimpMyunit\Resources\fire emblem images\" + almchar[rnd.Next(almchar.Length)] + ".png") { }
+        }
+
+        public void definepic6()
+        {
+            string[] almchar = new string[] {"Gray", "Tobin", "Faye", "Kliff", "Lukas", "Silque", "Clair", "Clive",
+                "Forsyth", "Python", "Luthier", "Mathilda", "Delthea", "Tatiana", "Zeke", "Mycen"};
+            Random rnd = new Random();
+            for (picBox6.ImageLocation = @"C:\Users\felip\source\repos\FEpimpMyunit\Resources\fire emblem images\" + almchar[rnd.Next(almchar.Length)] + ".png";
+                           picBox6.ImageLocation == picBox1.ImageLocation || picBox6.ImageLocation == picBox2.ImageLocation || picBox6.ImageLocation == picBox3.ImageLocation
+                           || picBox6.ImageLocation == picBox4.ImageLocation || picBox6.ImageLocation == picBox5.ImageLocation;
+                           picBox6.ImageLocation = @"C:\Users\felip\source\repos\FEpimpMyunit\Resources\fire emblem images\" + almchar[rnd.Next(almchar.Length)] + ".png") { }
+        }
+
+        public void definepic7()
+        {
+            string[] almchar = new string[] {"Gray", "Tobin", "Faye", "Kliff", "Lukas", "Silque", "Clair", "Clive",
+                "Forsyth", "Python", "Luthier", "Mathilda", "Delthea", "Tatiana", "Zeke", "Mycen"};
+            Random rnd = new Random();
+            for (picBox7.ImageLocation = @"C:\Users\felip\source\repos\FEpimpMyunit\Resources\fire emblem images\" + almchar[rnd.Next(almchar.Length)] + ".png";
+                           picBox7.ImageLocation == picBox1.ImageLocation || picBox7.ImageLocation == picBox2.ImageLocation || picBox7.ImageLocation == picBox3.ImageLocation
+                           || picBox7.ImageLocation == picBox4.ImageLocation || picBox7.ImageLocation == picBox5.ImageLocation || picBox7.ImageLocation == picBox6.ImageLocation;
+                           picBox7.ImageLocation = @"C:\Users\felip\source\repos\FEpimpMyunit\Resources\fire emblem images\" + almchar[rnd.Next(almchar.Length)] + ".png") { }
+        }
+
+        public void definepic8()
+        {
+            string[] almchar = new string[] {"Gray", "Tobin", "Faye", "Kliff", "Lukas", "Silque", "Clair", "Clive",
+                "Forsyth", "Python", "Luthier", "Mathilda", "Delthea", "Tatiana", "Zeke", "Mycen"};
+            Random rnd = new Random();
+            for (picBox8.ImageLocation = @"C:\Users\felip\source\repos\FEpimpMyunit\Resources\fire emblem images\" + almchar[rnd.Next(almchar.Length)] + ".png";
+                           picBox8.ImageLocation == picBox1.ImageLocation || picBox8.ImageLocation == picBox2.ImageLocation || picBox8.ImageLocation == picBox3.ImageLocation
+                           || picBox8.ImageLocation == picBox4.ImageLocation || picBox8.ImageLocation == picBox5.ImageLocation || picBox8.ImageLocation == picBox6.ImageLocation
+                           || picBox8.ImageLocation == picBox7.ImageLocation;
+                           picBox8.ImageLocation = @"C:\Users\felip\source\repos\FEpimpMyunit\Resources\fire emblem images\" + almchar[rnd.Next(almchar.Length)] + ".png") { }
+        }
+
+        public void definepic9()
+        {
+            string[] almchar = new string[] {"Gray", "Tobin", "Faye", "Kliff", "Lukas", "Silque", "Clair", "Clive",
+                "Forsyth", "Python", "Luthier", "Mathilda", "Delthea", "Tatiana", "Zeke", "Mycen"};
+            Random rnd = new Random();
+            for (picBox9.ImageLocation = @"C:\Users\felip\source\repos\FEpimpMyunit\Resources\fire emblem images\" + almchar[rnd.Next(almchar.Length)] + ".png";
+                           picBox9.ImageLocation == picBox1.ImageLocation || picBox9.ImageLocation == picBox2.ImageLocation || picBox9.ImageLocation == picBox3.ImageLocation
+                           || picBox9.ImageLocation == picBox4.ImageLocation || picBox9.ImageLocation == picBox5.ImageLocation
+                           || picBox9.ImageLocation == picBox6.ImageLocation || picBox9.ImageLocation == picBox7.ImageLocation || picBox9.ImageLocation == picBox8.ImageLocation;
+                           picBox9.ImageLocation = @"C:\Users\felip\source\repos\FEpimpMyunit\Resources\fire emblem images\" + almchar[rnd.Next(almchar.Length)] + ".png") { }
+        }
+
+        public void definepic10()
+        {
+            string[] almchar = new string[] {"Gray", "Tobin", "Faye", "Kliff", "Lukas", "Silque", "Clair", "Clive",
+                "Forsyth", "Python", "Luthier", "Mathilda", "Delthea", "Tatiana", "Zeke", "Mycen"};
+            Random rnd = new Random();
+            for (picBox10.ImageLocation = @"C:\Users\felip\source\repos\FEpimpMyunit\Resources\fire emblem images\" + almchar[rnd.Next(almchar.Length)] + ".png";
+                           picBox10.ImageLocation == picBox1.ImageLocation || picBox10.ImageLocation == picBox2.ImageLocation || picBox10.ImageLocation == picBox3.ImageLocation
+                           || picBox10.ImageLocation == picBox4.ImageLocation || picBox10.ImageLocation == picBox5.ImageLocation
+                           || picBox10.ImageLocation == picBox6.ImageLocation || picBox10.ImageLocation == picBox7.ImageLocation || picBox10.ImageLocation == picBox8.ImageLocation || picBox10.ImageLocation == picBox9.ImageLocation;
+                           picBox10.ImageLocation = @"C:\Users\felip\source\repos\FEpimpMyunit\Resources\fire emblem images\" + almchar[rnd.Next(almchar.Length)] + ".png") { }
+        }
+
+      }
+}
